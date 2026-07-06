5368817357799f45fbf0d38ebe86b3fa159cfaff
Kisa-San-Reader <kisa-san-reader@users.noreply.github.com>
2026-07-06 13:48:00 -0600

    fix sw paths for github pages

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'motivate-me-v2-cache-v2';
+const CACHE_NAME = 'motivate-me-v2-cache-v3';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
